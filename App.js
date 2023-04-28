f7ca90e15a4fafa4cb9424300190b05d8368e72e
LaurenceLama <laurencelamaFSE@gmail.com>
2023-04-28 23:28:11 +0800

    Initialized completion of firebase exercise, could not debugged by myself tho, nt

--- App.js
+++ App.js
@@ -11,13 +11,14 @@ import "./App.css";
 function App() {
   const [user, setUser] = React.useState({})
   const [loading, setLoading] = React.useState(true)
+  const [loggedIn, setLoggedIn] = React.useState(false);
 
   React.useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       setLoading(false);
-      console.log(user)
       if (user) {
         setUser(user)
+        setLoggedIn(true);
       }
     })
   }, []) //useEffect command for automatic logins when refreshing page
@@ -49,15 +50,28 @@ function App() {
   function logout() {
     signOut(auth);
     setUser({})
+    setLoggedIn(false)
   }
 
   return (
-    <div className="App">
-      <button onClick={register}>Register</button>
-      <button onClick={login}>Login</button>
-      <button onClick={logout}>Logout</button>
-      {loading ? 'loading... WAIT' : user.email}
-    </div>
+    <>
+      <div className="intro">
+        <button onClick={register}>Register</button>
+        <button onClick={login}>Login</button>
+        <button onClick={logout}>Logout</button>
+        {loading ? "loading... WAIT" : user.email}
+      </div>
+
+      <br></br>
+      {/* exercise wants to logout using the mounted initial of the acc name, then also add another loading state for that logout function */}
+      <div className="exercise">
+        {!loggedIn && <button onClick={register}>Register</button>}
+        {!loggedIn && <button onClick={login}>Login</button>}
+        {loggedIn && (
+          <button onClick={logout}>{user.email[0].toUpperCase()}</button>
+        )}
+      </div>
+    </>
   );
 }
 
